c560d2aa945d57ab228abc8990d32d57571897d7
湫曗 <autumnsilverleaves@gmail.com>
2022-03-20 21:05:39 +0800

    feat: add redirects to netlify.toml

--- packages/example/src/index.ts
+++ packages/example/src/index.ts
@@ -2,6 +2,6 @@ import './style.css'
 // import 'virtual:windi-devtools'
 import 'virtual:windi.css'
 import { createApp } from 'vue'
-import { App } from './app'
+import { App } from './App'
 const app = createApp(App)
 app.mount('#app')
