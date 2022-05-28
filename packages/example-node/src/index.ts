10ec54afd2aed5e2ab1f5b83b0103394298deda3
湫曗 <autumnsilverleaves@gmail.com>
2022-05-28 15:30:30 +0800

    feat: up node example

--- packages/example-node/src/index.ts
+++ packages/example-node/src/index.ts
@@ -1,1 +1,10 @@
-console.log('hello world!')
+import { resolve } from 'path'
+import { promises } from 'fs'
+
+console.log('hello world!\n')
+
+console.log('this file content:')
+promises.readFile(resolve(__dirname, './index.ts')).then((value) => {
+  console.log(value.toString())
+})
+console.log('-----------')
