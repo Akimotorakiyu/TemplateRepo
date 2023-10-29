dae837e64dd3df8c14edb39db6e1d5de773527b6
湫曗 <18817832003@qq.com>
2023-10-29 11:14:49 +0800

    feat: up dep

--- packages/example-node/src/index.ts
+++ packages/example-node/src/index.ts
@@ -1,10 +1,11 @@
-import { resolve } from 'path'
-import { promises } from 'fs'
+type TFact = (fatc: TFact, n: number) => number
+// Fact
+const fact4 = ((n: number) => {
+  return ((fact: TFact) => {
+    return fact(fact, n)
+  })((fact: TFact, n: number) => {
+    return n===0?1: n * fact(fact, n - 1)
+  })
+})(4)
 
-console.log('hello world!\n')
-
-console.log('this file content:')
-promises.readFile(resolve(__dirname, './index.ts')).then((value) => {
-  console.log(value.toString())
-})
-console.log('-----------')
+console.log(`4! = ${fact4}`)
