5afd99fedb53ee7416f2a50e25dca048a47c6018
湫曗 <autumnsilverleaves@gmail.com>
2024-09-21 15:32:04 +0800

    feat: nnode  support polars

--- packages/example-node/src/index.ts
+++ packages/example-node/src/index.ts
@@ -1,10 +1,11 @@
+import './polars'
 type TFact = (fatc: TFact, n: number) => number
 // Fact
 const fact4 = ((n: number) => {
   return ((fact: TFact) => {
     return fact(fact, n)
   })((fact: TFact, n: number) => {
-    return n===0?1: n * fact(fact, n - 1)
+    return n === 0 ? 1 : n * fact(fact, n - 1)
   })
 })(4)
 
