b12445ad1d2f56f20b800a47475a3534dbc2e215
湫曗 <autumnsilverleaves@gmail.com>
2024-09-21 15:27:21 +0800

    feat: use setup

---
+++ packages/example-vue/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>hello world</div>
+  <div>{{ greeting }}</div>
+  <div>{{ time }}</div>
+</template>
+<script setup lang="ts">
+import { ref } from 'vue'
+import { getGreeting } from '@template/template'
+const time = ref(new Date())
+const greeting = ref(getGreeting())
+
+setInterval(() => {
+  time.value = new Date()
+}, 1000)
+</script>
--- packages/example-vue/src/index.ts
+++ packages/example-vue/src/index.ts
@@ -2,6 +2,6 @@ import './style.css'
 // import 'virtual:windi-devtools'
 import 'virtual:windi.css'
 import { createApp } from 'vue'
-import { App } from './App'
+import App from './App.vue'
 const app = createApp(App)
 app.mount('#app')
