f5106ea43c9e85a0d34bac5afabf57bcb28e7dca
湫曗 <18817832003@qq.com>
2026-01-07 00:48:23 +0800

    feat: use stylex

--- packages/react/src/App.tsx
+++ packages/react/src/App.tsx
@@ -1,5 +1,28 @@
 import { getGreeting } from '@template/template'
+import * as stylex from '@stylexjs/stylex'
+
+const styles = stylex.create({
+  container: {
+    fontSize: '2rem',
+    fontWeight: 'bold',
+    color: 'blue',
+    padding: '20px',
+    backgroundColor: '#f0f0f0',
+    borderRadius: '8px',
+    textAlign: 'center',
+    marginTop: '50px',
+  },
+  reactText: {
+    color: 'red',
+    fontWeight: 'lighter',
+  },
+})
 
 export const App = () => {
-  return <div>{getGreeting()} --react</div>
+  return (
+    <div {...stylex.props(styles.container)}>
+      {getGreeting()}
+      <span {...stylex.props(styles.reactText)}> --react</span>
+    </div>
+  )
 }
